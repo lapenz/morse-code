87c48d6ebd776e3290684e8c0738f6496a898ba4
lucaspenz <lucasarthurpenz@gmail.com>
2025-01-25 00:52:15 -0500

    Github pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    output: 'export'
+    output: 'export',
+    basePath: '/morse-code',
+    assetPrefix: '/morse-code/',
+    images: {
+        unoptimized: true, // Required if you are using Next.js Image Optimization
+    }
 };
 
 export default nextConfig;
