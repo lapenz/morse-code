5e0805485c41b0b5c927854edce289688140b3cb
lucaspenz <lucasarthurpenz@gmail.com>
2025-01-25 01:16:33 -0500

    Github pages

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ const nextConfig = {
     output: 'export',
     trailingSlash: true,
     basePath: '/morse-code', // GitHub repository name
-    assetPrefix: '/morse-code', // Prefix for static assets
+    assetPrefix: '/morse-code/', // Prefix for static assets
 };
 
 export default nextConfig;
