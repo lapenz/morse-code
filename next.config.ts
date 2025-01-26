ee0b51763aa4473970ae11d31fc067573e6eacb5
lucaspenz <lucasarthurpenz@gmail.com>
2025-01-25 22:47:13 -0500

    Nojekyll fix Github pages

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    trailingSlash: true,
-    basePath: '/morse-code', // GitHub repository name
-    assetPrefix: '/morse-code/', // Prefix for static assets
+    output: "export", // Enable static export
+    trailingSlash: true, // Add trailing slashes to paths
+    basePath: "/morse-code", // Use your GitHub repo name
+    assetPrefix: "/morse-code/", // Prefix for static assets (ensure the trailing slash)
 };
 
-export default nextConfig;
+module.exports = nextConfig;
